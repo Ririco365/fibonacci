0cf89b5c7d38620971dc02befad9ae00931c8a9c
Ririco365 <pinchy2525@yahoo.co.jp>
2018-02-07 00:39:48 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 'use strict';
-function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+const memo = new Map(); // 連想配列。key:順番、value:フィボナッチの値
+memo.set(0, 0);
+memo.set(1, 1);
+function fib(n){
+    //もし連想配列が、n をキーとした答えを持っていればその値をそのまま関数の値として返す
+    if (memo.has(n)) {
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+    //そうでない場合は再帰関数を計算して値を求め、それを連想配列に格納した後に返す
+    const value = fib(n - 1) + fib (n - 2);
+    memo.set(n, value);
+    return value; 
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
